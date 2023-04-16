8875922d2f782c418404c7999d1ff2ec53e0a87a
shubhpatil123 <shubhampatil14896@gmail.com>
2023-04-16 11:38:31 +0530

    Added Checking ODD or EVEN

--- Odd_even.py
+++ Odd_even.py
@@ -2,3 +2,7 @@ a=int(input('Enter First Number: '))
 b=int(input('Enter Second Number: '))
 c=a+b
 print("Total is :"+str(c))
+if(c%2==0):
+    print("It is Even Number")
+else:
+    print("It is ODD Number")
